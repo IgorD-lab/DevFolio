98eb47c9565fda82365ee5de88965ea5ee651f4f
King1 <igordobricc@gmail.com>
2024-04-30 22:22:20 +0200

    Pagination

--- projects/models.py
+++ projects/models.py
@@ -18,6 +18,9 @@ class Project(models.Model):
     
     def __str__(self):
         return self.title
+    
+    class Meta:
+        ordering = ['-created']
 
 class Review(models.Model):
     VOTE_TYPE = (
--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from .models import Profile
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
-from .utils import searchProfiles
+from .utils import searchProfiles, paginateProfiles
 
 
 def loginUser(request):
@@ -65,7 +65,9 @@ def registerUser(request):
 def profiles(request): 
     profiles, search_query = searchProfiles(request)
     
-    context = {'profiles': profiles, 'search_query': search_query}
+    custom_range, profiles = paginateProfiles(request, profiles, 6)
+    
+    context = {'profiles': profiles, 'search_query': search_query, 'custom_range': custom_range}
     return render(request, 'users/profiles.html', context)
 
 def userProfile(request, pk):
