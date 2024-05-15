c2a9ee08862be1a78bde43debc528b5e9502f847
Igor D. <igordobricc@gmail.com>
2024-05-15 16:42:09 +0100

    AWS issues work paused for now

--- devfolio/settings.py
+++ devfolio/settings.py
@@ -104,10 +104,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'devfolio.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'devfind',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
